870d8232a19bf6d73fcd9d8958965adaf823d7ff
juha tervo <juha.tervo@aalto.fi>
2020-04-24 10:20:11 +0300

    init

---
+++ build/ball_chaser/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/ros_ws/u_robo/src/ball_chaser
+# Build directory: /home/ubuntu/ros_ws/u_robo/build/ball_chaser
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
